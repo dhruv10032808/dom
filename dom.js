c529f5fb5e947abcb6e346a673e45dd083507580
dhruv10032808 <dhruv.bhanu.1003@gmail.com>
2022-12-27 13:55:07 +0530

    commit 2

--- dom.js
+++ dom.js
@@ -1,1 +1,16 @@
 console.log("hello");
+console.log(document.title);
+console.log(document.head);
+document.title="red";
+console.log(document.forms);
+console.log(document.images);
+//getElementById
+//var a=document.getElementById("items");
+//console.log(a);
+//a.innerText="orange";
+var h=document.getElementById("header-title");
+//h.innerHTML='<h5>Vamos</h5>';
+h.style.border='2px solid black';
+var n=document.querySelector(".title");
+n.style.color='green';
+n.style.fontWeight='bold';
