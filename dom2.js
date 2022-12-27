fb3667856684f5a9606681cb2c8becd56f4256b5
dhruv10032808 <dhruv.bhanu.1003@gmail.com>
2022-12-28 02:34:22 +0530

    search commit

--- dom2.js
+++ dom2.js
@@ -1,15 +1,31 @@
 var form=document.getElementById('addForm');
 var newitem =document.getElementById('items');
+var filter=document.getElementById('filter');
+filter.addEventListener('keyup',search);
+function search(e){
+    var value=e.target.value.toLowerCase();
+    var ele=newitem.getElementsByTagName('li');
+    Array.from(ele).forEach(function(el){
+        var init=el.firstChild.textContent;
+        var fin=el.childNodes[1].textContent;
+        if(init.toLowerCase().indexOf(value)!=-1||fin.toLowerCase().indexOf(value)!=-1){
+            el.style.display='block';
+        }else{
+            el.style.display='none';
+        }
+    });
+}
 newitem.addEventListener('click',remove);
 form.addEventListener('submit', addItem);
 function addItem(e){ 
 e.preventDefault();
-
+var de=document.getElementById('desc').value;
 var newItem=document.getElementById('item').value;
 var li=document.createElement('li');
 li.className = 'list-group-item';
 console.log(li);
 li.appendChild(document.createTextNode(newItem));
+li.appendChild(document.createTextNode(" "+de));
 //create delete  button
 var del=document.createElement('button');
 del.className="btn btn-danger btn-sm float-right delete";
